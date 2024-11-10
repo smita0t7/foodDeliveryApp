ea2fe100cb51bd4272ea06ef8bec1f28a5afb98f
SmitaSriyaSarangi <sdvmcsc@gmail.com>
2024-11-10 14:57:29 +0530

    navbar is set

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-
 import './App.css';
 import Home from './screens/Home';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom"
+import Login from './screens/Login';
 //multiple divs are not allowed here
 function App() {
   return (
-    <>
-   <div > <Home/> </div>  
-   </>
+    <Router>
+   <div > 
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/login" element={<Login />} />
+    </Routes>
+   </div>  
+   </Router>
   );
 }
 export default App;
